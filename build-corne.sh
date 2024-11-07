ac64047574413554f34930ac9860b5230d103dba
David Gallmeier <dg@xounges.net>
2024-11-07 00:43:30 -0500

    add discussion url to customize unused keys

--- build-corne.sh
+++ build-corne.sh
@@ -1,6 +1,6 @@
 #!/bin/env sh
 
-qmk flash -c -kb foostan/cornelius -km manna-harbour_miryoku \
+qmk flash -c -kb crkbd/rev1 -km manna-harbour_miryoku \
     -e CONVERT_TO=rp2040_ce \
     -e MIRYOKU_ALPHAS=QWERTY \
     -e MIRYOKU_NAV=VI
