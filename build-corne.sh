a42240b2c0682a07bfd77cd1cc05cea0c85b9f45
David Gallmeier <dg@xounges.net>
2024-11-06 22:32:19 -0500

    add build corne command

---
+++ build-corne.sh
@@ -0,0 +1,8 @@
+#!/bin/env sh
+
+qmk flash -c -kb foostan/cornelius -km manna-harbour_miryoku \
+    -e CONVERT_TO=rp2040_ce \
+    -e MIRYOKU_ALPHAS=QWERTY \
+    -e MIRYOKU_NAV=VI
+
+# https://docs.qmk.fm/feature_converters#converters
